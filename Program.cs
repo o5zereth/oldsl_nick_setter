f30d7024753f019968dc8de885b710e8bf14ac02
o5zereth <jthomaspiper2006@gmail.com>
2021-05-22 07:22:23 -0400

    v1.0.4
    added some extra checks to prevent malicious people making names with a billion characters

--- Program.cs
+++ Program.cs
@@ -18,9 +18,21 @@ namespace oldsl_nick_setter
                 Console.WriteLine("Please write your desired nickname.");
                 string nick = Console.ReadLine();
 
-                if (string.IsNullOrWhiteSpace(nick) || nick.Length < 3)
+                int totalNonWhiteSpace = 0;
+
+                for (int i = 0; i < nick.Length; i++)
                 {
-                    WriteLineColor("The entered nickname is not allowed. Make sure the nickname consists of atleast 3 characters and is not whitespace only.", ConsoleColor.Magenta);
+                    if (!string.IsNullOrWhiteSpace($"{nick[i]}"))
+                    {
+                        totalNonWhiteSpace++;
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(nick) || totalNonWhiteSpace < 3 || nick.Length > 25)
+                {
+                    WriteLineColor("The entered nickname is not allowed.", ConsoleColor.DarkRed);
+                    WriteLineColor("Make sure the nickname is between 3-25 characters and is not whitespace only.", ConsoleColor.Magenta);
+                    
                     Console.WriteLine("\nPress any key to exit.");
                     Console.ReadKey();
                     return;
