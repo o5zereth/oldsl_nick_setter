772eaf84d4cbd10c31556405c6604226dc214b8b
Zereth <70236765+o5zereth@users.noreply.github.com>
2021-08-02 19:33:39 -0400

    Update Program.cs
    Add code comments for those who want more insight into the program.

--- Program.cs
+++ Program.cs
@@ -6,20 +6,24 @@ namespace oldsl_nick_setter
 {
     class Program
     {
+        // Registry keynames for oldest versions of SCP:SL.
         private const string keyName = @"HKEY_CURRENT_USER\Software\Hubert Moszka\SCP: Secret Laboratory";
-        private const string keyName_2 = @"HKEY_CURRENT_USER\Software\Hubert Moszka\SCPSL";
         private const string subKeyName = @"Software\Hubert Moszka\SCP: Secret Laboratory";
+
+        // Registry keynames for older versions of SCP:SL.
+        private const string keyName_2 = @"HKEY_CURRENT_USER\Software\Hubert Moszka\SCPSL";
         private const string subKeyName_2 = @"Software\Hubert Moszka\SCPSL";
 
         static void Main(string[] args)
         {
             try
             {
+                // Get the written nickname.
                 Console.WriteLine("Please write your desired nickname.");
                 string nick = Console.ReadLine();
 
+                // Get the number of total whitespace characters.
                 int totalNonWhiteSpace = 0;
-
                 for (int i = 0; i < nick.Length; i++)
                 {
                     if (!string.IsNullOrWhiteSpace($"{nick[i]}"))
@@ -28,16 +32,20 @@ namespace oldsl_nick_setter
                     }
                 }
 
+                // Don't allow nicknames that are empty, contain less than 3 non-whitespace characters, or exceed 25 characters in length.
                 if (string.IsNullOrWhiteSpace(nick) || totalNonWhiteSpace < 3 || nick.Length > 25)
                 {
+                    // Tell user what is allowed.
                     WriteLineColor("The entered nickname is not allowed.", ConsoleColor.DarkRed);
                     WriteLineColor("Make sure the nickname is between 3-25 characters and is not whitespace only.", ConsoleColor.Magenta);
-                    
+
+                    // Exit the program on key-press.
                     Console.WriteLine("\nPress any key to exit.");
                     Console.ReadKey();
                     return;
                 }
 
+                // Deletes all currently saved nicknames for oldest versions of SCP:SL.
                 using (RegistryKey key = Registry.CurrentUser.OpenSubKey(subKeyName, true))
                 {
                     if (key != null)
@@ -54,6 +62,7 @@ namespace oldsl_nick_setter
                     }
                 }
 
+                // Deletes all currently saved nicknames for older versions of SCP:SL.
                 using (RegistryKey key = Registry.CurrentUser.OpenSubKey(subKeyName_2, true))
                 {
                     if (key != null)
@@ -70,16 +79,19 @@ namespace oldsl_nick_setter
                     }
                 }
 
+                // Add given nickname to both old SCP:SL keynames.
                 Registry.SetValue(keyName, "nickname", Encoding.ASCII.GetBytes(nick));
                 Registry.SetValue(keyName_2, "nickname", Encoding.ASCII.GetBytes(nick));
 
+                // Tell user the nickname was set.
                 WriteLineColor("Nickname set succssfully!", ConsoleColor.Green);
             }
-            catch (Exception e)
+            catch (Exception e) // If an error occurs, then log it.
             {
                 WriteLineColor(e.ToString(), ConsoleColor.DarkRed);
             }
 
+            // Exit the program on key-press.
             Console.WriteLine("\nPress any key to exit.");
             Console.ReadKey();
         }
